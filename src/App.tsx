2168cdecdb0bc345963c975a8bbe1d4642830fb2
Edan Bainglass <edan.bainglass@gmail.com>
2024-07-15 07:25:56 +0200

    Test via `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import PhononVisualizer from "./components/PhononVisualizer";
+
+import data from "../data/test.json";
+
 import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    // @ts-expect-error otherwise, each part of data would need unpacking
+    <PhononVisualizer props={{ title: "Demo", ...data }} />
+  );
 }
 
 export default App;
