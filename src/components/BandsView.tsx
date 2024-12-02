432e9aa2b09cee6a5c5c1c1ae8ae0fbc2eacdc66
Kristjan Eimre <kristjaneimre@gmail.com>
2024-12-03 01:18:39 +0200

    use more state

--- src/components/BandsView.tsx
+++ src/components/BandsView.tsx
@@ -1,10 +1,114 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import Plot from "react-plotly.js";
 
 import { PlotDatum, PlotMouseEvent } from "plotly.js";
 
 import { HighSymPoint } from "./types";
+
+const getLayout = (
+  highSymPoints: HighSymPoint[],
+  distances: number[],
+  eigenvalues: number[][]
+) => ({
+  showlegend: false,
+  hovermode: "closest",
+  xaxis: {
+    linewidth: 0,
+    linecolor: "transparent",
+    tickvals: highSymPoints.map(([index]) => distances[index]),
+    ticktext: highSymPoints.map(([, label]) => label),
+    range: [Math.min(...distances.flat()), Math.max(...distances.flat())],
+  },
+  yaxis: {
+    title: "Frequency (cm-1)",
+    linewidth: 0,
+    linecolor: "transparent",
+    ticklen: 5,
+    range: [Math.min(...eigenvalues.flat()), Math.max(...eigenvalues.flat())],
+  },
+  // vertical lines at high-symmetry points
+  shapes: highSymPoints.map(([index]) => ({
+    type: "line",
+    yref: "paper",
+    x0: distances[index],
+    y0: 0,
+    x1: distances[index],
+    y1: 1,
+    line: {
+      width: 0.5,
+    },
+  })),
+  dragmode: "zoom",
+  autosize: true,
+  margin: {
+    l: 55,
+    r: 10,
+    b: 25,
+    t: 40,
+  },
+});
+
+const getPlotData = (
+  bands: number[][],
+  distances: number[],
+  hoveredPoint: PlotDatum | null,
+  selectedPoint: PlotDatum | null
+) => {
+  return bands.map((band, index) => ({
+    x: distances,
+    y: band,
+    mode: "lines+markers",
+    hoverinfo: "none",
+    line: {
+      color: "#1f77b4",
+      width: hoveredPoint?.curveNumber === index ? 4 : 2,
+    },
+    marker: {
+      // size should be 2 if hovered on, 4 if selected, 0 otherwise; hover/selection is determined by both x and y, no border
+      size: band.map((_, index) =>
+        selectedPoint?.x === distances[index] &&
+        selectedPoint?.y === band[index]
+          ? 10
+          : hoveredPoint?.x === distances[index] &&
+            hoveredPoint?.y === band[index]
+          ? 14
+          : 0
+      ),
+      color: band.map((_, index) =>
+        selectedPoint?.x === distances[index] &&
+        selectedPoint?.y === band[index]
+          ? "red"
+          : hoveredPoint?.x === distances[index] &&
+            hoveredPoint?.y === band[index]
+          ? "blue"
+          : "#1f77b4"
+      ),
+      // add opaque border around marker of width 1 (black) if selected, width 10 (lightblue) if hovered on
+      line: {
+        width: band.map((_, index) =>
+          selectedPoint?.x === distances[index] &&
+          selectedPoint?.y === band[index]
+            ? 1
+            : hoveredPoint?.x === distances[index] &&
+              hoveredPoint?.y === band[index]
+            ? 8
+            : 0
+        ),
+        color: band.map((_, index) =>
+          selectedPoint?.x === distances[index] &&
+          selectedPoint?.y === band[index]
+            ? "black"
+            : hoveredPoint?.x === distances[index] &&
+              hoveredPoint?.y === band[index]
+            ? "lightblue"
+            : "transparent"
+        ),
+      },
+      opacity: 1,
+    },
+  }));
+};
 
 const BandsView = ({
   distances,
@@ -24,6 +128,25 @@ const BandsView = ({
     eigenvalues.map((row) => row[colIndex])
   );
 
+  const [plotState, setPlotState] = useState({
+    data: getPlotData(bands, distances, hoveredPoint, selectedPoint),
+    layout: getLayout(highSymPoints, distances, eigenvalues),
+    frames: [],
+    config: {
+      scrollZoom: false,
+      displayModeBar: true,
+      displaylogo: false,
+      modeBarButtons: [["toImage", "resetScale2d"]],
+    },
+  });
+
+  useEffect(() => {
+    setPlotState((oldState) => ({
+      ...oldState,
+      data: getPlotData(bands, distances, hoveredPoint, selectedPoint),
+    }));
+  }, [hoveredPoint, selectedPoint]);
+
   const handleSelection = (event: PlotMouseEvent) => {
     updateMode(event);
     setSelectedPoint(event.points[0]);
@@ -34,115 +157,15 @@ const BandsView = ({
       <Card.Header>Phonon band structure (select phonon)</Card.Header>
       <Card.Body>
         <Plot
-          data={bands.map((band, index) => ({
-            x: distances,
-            y: band,
-            mode: "lines+markers",
-            hoverinfo: "none",
-            line: {
-              color: "#1f77b4",
-              width: hoveredPoint?.curveNumber === index ? 4 : 2,
-            },
-            marker: {
-              // size should be 2 if hovered on, 4 if selected, 0 otherwise; hover/selection is determined by both x and y, no border
-              size: band.map((_, index) =>
-                selectedPoint?.x === distances[index] &&
-                selectedPoint?.y === band[index]
-                  ? 10
-                  : hoveredPoint?.x === distances[index] &&
-                    hoveredPoint?.y === band[index]
-                  ? 14
-                  : 0
-              ),
-              color: band.map((_, index) =>
-                selectedPoint?.x === distances[index] &&
-                selectedPoint?.y === band[index]
-                  ? "red"
-                  : hoveredPoint?.x === distances[index] &&
-                    hoveredPoint?.y === band[index]
-                  ? "blue"
-                  : "#1f77b4"
-              ),
-              // add opaque border around marker of width 1 (black) if selected, width 10 (lightblue) if hovered on
-              line: {
-                width: band.map((_, index) =>
-                  selectedPoint?.x === distances[index] &&
-                  selectedPoint?.y === band[index]
-                    ? 1
-                    : hoveredPoint?.x === distances[index] &&
-                      hoveredPoint?.y === band[index]
-                    ? 8
-                    : 0
-                ),
-                color: band.map((_, index) =>
-                  selectedPoint?.x === distances[index] &&
-                  selectedPoint?.y === band[index]
-                    ? "black"
-                    : hoveredPoint?.x === distances[index] &&
-                      hoveredPoint?.y === band[index]
-                    ? "lightblue"
-                    : "transparent"
-                ),
-              },
-              opacity: 1,
-            },
-          }))}
-          layout={{
-            showlegend: false,
-            hovermode: "closest",
-            xaxis: {
-              linewidth: 0,
-              linecolor: "transparent",
-              tickvals: highSymPoints.map(([index]) => distances[index]),
-              ticktext: highSymPoints.map(([, label]) => label),
-              range: [
-                Math.min(...distances.flat()),
-                Math.max(...distances.flat()),
-              ],
-            },
-            yaxis: {
-              title: "Frequency (cm-1)",
-              linewidth: 0,
-              linecolor: "transparent",
-              ticklen: 5,
-              range: [
-                Math.min(...eigenvalues.flat()),
-                Math.max(...eigenvalues.flat()),
-              ],
-            },
-            // vertical lines at high-symmetry points
-            shapes: highSymPoints.map(([index]) => ({
-              type: "line",
-              yref: "paper",
-              x0: distances[index],
-              y0: 0,
-              x1: distances[index],
-              y1: 1,
-              line: {
-                width: 0.5,
-              },
-            })),
-            dragmode: "zoom",
-            autosize: true,
-            margin: {
-              l: 55,
-              r: 10,
-              b: 25,
-              t: 40,
-            },
-          }}
+          data={plotState.data}
+          layout={plotState.layout}
+          config={plotState.config}
           onClick={handleSelection}
           onHover={(event) => {
             setHoveredPoint(event.points[0]);
           }}
           onUnhover={() => {
             setHoveredPoint(null);
-          }}
-          config={{
-            scrollZoom: false,
-            displayModeBar: true,
-            displaylogo: false,
-            modeBarButtons: [["toImage", "resetScale2d"]],
           }}
           useResizeHandler={true}
           style={{
