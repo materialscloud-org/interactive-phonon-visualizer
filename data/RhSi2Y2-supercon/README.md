b0c9b3643e9016e6b1952cfa79e69e6ea43e2a86
Kristjan Eimre <kristjaneimre@gmail.com>
2025-10-20 15:52:48 +0200

    Redesign python package: phonon-web-tools

---
+++ data/RhSi2Y2-supercon/README.md
@@ -0,0 +1,10 @@
+This data is taken from the MC3D superconductivity contribution.
+
+```python
+matdyn_retrieved_uuid = "4d458ee8-79e6-4939-92bf-ff6540cd0f56"
+pw_workchain_uuid = "9be2414a-1d4b-47b5-9a26-8ae3a86f07eb"
+```
+
+The Q-point labels were taken from the associated phonon bandstructure calculation.
+
+Note that the automatic seekpath label generation doesn't work in this case (probably due to a different symprec).
---
+++ phonon-web-tools/run_examples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+from phonon_web_tools import convert_qe_phonon_folder
+
+base_folder = Path(__file__).parent.parent / "./data"
+
+required_files = {"scf.in", "scf.out", "matdyn.modes"}
+
+for folder in base_folder.iterdir():
+    if folder.is_dir():
+        files = {f.name for f in folder.iterdir() if f.is_file()}
+        if required_files.issubset(files):
+            print(f"Converting {folder}")
+            convert_qe_phonon_folder(folder)
